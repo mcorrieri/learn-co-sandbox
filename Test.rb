f87bb9d732e140118af41f4e2740560aa52b578e
Michael Corrieri <mlcorrieri@gmail.com>
2021-03-09 01:05:35 +0000

    Automatically backed up by Learn

---
+++ Test.rb
@@ -0,0 +1,10 @@
+answer = "< fill in your answer here >"
+puts "A group of flamingos is called a #{answer}."
+answer = "flamboyance"
+puts "A group of flamingos is called a #{answer}."
+
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
